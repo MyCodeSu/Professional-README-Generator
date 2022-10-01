50e28886b06fa7a5925c7ab2982f399cc97033c5
michaelsu01@gmail.com <michaelsu01@gmail.com>
2022-10-01 18:55:42 -0400

    question prompts added.

--- Develop/index.js
+++ Develop/index.js
@@ -22,13 +22,163 @@ const generateMarkdown = require('./Develop/utils/generateMarkdown.js');
 const util = require('util');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your repository? (Required)',
+    validate: nameInput => {
+        if (nameInput) {
+            return true;
+        } else {
+            console.log('Please enter your repository title.');
+            return false;
+        }
+    }
+},
+{
+    type: 'input',
+    name: 'description',
+    message: 'Describe your repository? (Required)',
+    validate: nameInput => {
+        if (nameInput) {
+            return true;
+        } else {
+            console.log('Please enter a description of the repository.');
+            return false;
+        }
+    }
+},
+{
+    type: 'confirm',
+    name: 'confirmInstallation',
+    message: 'Is there an installation process?'
+},
+{
+    type: 'input',
+    name: 'installation',
+    message: 'Please list installation instructions.',
+    // the <when> is a condition that allows the user to input an installation process if installation is required.
+    when: ({ confirmInstallation }) => {
+      if (confirmInstallation) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  },
+  {
+  type: 'confirm',
+  name: 'confirmUsage',
+  message: 'Would you like to provide instructions on how to use your application?'
+  },
+  { //if confirmed
+    type: 'input',
+    name: 'instructions',
+    message: 'Please provide instructions on how to use your application.',
+    when: ({ confirmUsage }) => {
+      if (confirmUsage) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  },
+  {
+    type: 'confirm',
+    name: 'confirmContribution',
+    message: 'Will other developers be granted permission to contribute to your repository?'
+  },
+  {
+    type: 'input',
+    name: 'contribution',
+    message: 'Please describe how other developers can contribute to your project.',
+    when: ({ confirmContribution }) => {
+      if (confirmContribution) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  },
+  {
+    type: 'confirm',
+    name: 'testConfirm',
+    message: 'Is testing available?'
+  },
+  {
+    type: 'input',
+    name: 'testing',
+    message: 'Please describe how users may test your application.',
+    when: ({ testConfirm }) => {
+      if (testConfirm) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  },
+  { //checkbox that allows license choice
+    type: 'checkbox',
+    name: 'license',
+    message: 'Please choose a license type.',
+    choices: ['GNU AGPLv3', 'GNU GPLv3',
+      'GNU LGPLv3', 'Mozilla Public License 2.0',
+      'Apache License 2.0', 'MIT License', 'Boost Software License 1.0',
+      'The Unlicense'],
+    validate: nameInput => {
+      if (nameInput) {
+        return true;
+      } else {
+        console.log('Please select a license type.');
+        return false;
+      }
+    }
+  },
+  {
+    type: 'input',
+    name: 'username',
+    message: 'What is your GitHub username? (Required)',
+    validate: nameInput => {
+      if (nameInput) {
+        return true;
+      } else {
+        console.log('Please enter your GitHub username.');
+        return false;
+      }
+    }
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'What is your email address? (Required)',
+    validate: nameInput => {
+      if (nameInput) {
+        return true;
+      } else {
+        console.log('Please enter your email.');
+        return false;
+      }
+    }
+  },
+  {
+    type: 'input',
+    name: 'questions',
+    message: 'Please provide instructions on how you prefer to be contacted.',
+    validate: (nameInput) => {
+      if (nameInput) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }];
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
